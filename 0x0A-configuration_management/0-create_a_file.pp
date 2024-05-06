48aae2397c4561443692dfedffdae3274a53f72f
josMuiruri <josemuiruri51@gmail.com>
2024-05-06 15:45:25 +0300

    addition of a readme

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,11 @@
+# Modify client config file
+$file = '/tmp/school'
+
+#create doc root directory
 file { '/tmp/school':
-	ensure => file,
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => "I love Puppet\n",
+   ensure => file,
+   mode => '0744',
+   owner => 'www-data',
+   group => 'www-data',
+   content => "I love Puppet\n",
 }
